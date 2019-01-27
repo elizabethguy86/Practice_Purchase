07cb8dee2e8b5f125ce3e73dfadf3c90a3948164
Joey Gaitan <joeygaitan707@gmail.com>
2019-01-26 16:20:40 -0800

    finished

--- site/backend/MVC/controllers/info.js
+++ site/backend/MVC/controllers/info.js
@@ -2,7 +2,7 @@ const model = require('../models/info')
 
 const getAll = (req,res,next) => {
     const data = model.getAll()
-
+    
     if(data.error){
         return next({status: 404, message: `blogs not found`, error: data.error})
     }
--- site/backend/MVC/models/info.js
+++ site/backend/MVC/models/info.js
@@ -1,14 +1,16 @@
 const file = require('./filesync')
-const info = require('./data/info')
+const info = require('./data/chris')
+const info2 = require('./data/sarah')
+const info3 = require('./data/karl')
 
 function getAll(){
-    const blogs = file.filesync('read', '/blogs.json')
+    const info = file.filesync('read', '/chris.json')
+    const info2 = file.filesync('read', '/karl.json')
+    const info3 = file.filesync('read', '/sarah.json')
     const error = []
 
-    if(!blogs){
-        error.push('no blogs found')
-        return { error }
-    }
-
-    return { blogs }
+    console.log({ })
+    return { info, info2, info3 }
 }
+
+module.exports = { getAll }
