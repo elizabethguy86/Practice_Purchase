47ded6492034cc87e5499a1f3e94279bebeafd83
Joey Gaitan <joeygaitan707@gmail.com>
2019-01-26 17:02:27 -0800

    push

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3002
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
